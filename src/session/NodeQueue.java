b2c756b8896edb4756738327bf9acee6758b5722
rjsvcs <rjsvcs@rit.edu>
2019-10-22 09:03:49 -0400

    doc finished

--- src/session/NodeQueue.java
+++ src/session/NodeQueue.java
@@ -1,11 +1,29 @@
 package session;
 
+/**
+ * A node-based implementation of a queue.
+ *
+ * @param <T> The type of values stored in the queue.
+ */
 public class NodeQueue<T> implements Queue<T> {
-
+    /**
+     * The front of the queue. This contains the oldest value in the queue.
+     */
     private Node<T> front;
+
+    /**
+     * The back of the queue. This contains the newest value in the queue.
+     */
     private Node<T> back;
+
+    /**
+     * The number of values in the queue.
+     */
     private int size;
 
+    /**
+     * Creates a new, empty queue.
+     */
     public NodeQueue() {
         front = null;
         back = null;
@@ -14,27 +32,37 @@ public class NodeQueue<T> implements Queue<T> {
 
     @Override
     public void enqueue(T value) {
+        // create a new node
         Node<T> node = new Node<>(value);
+        // if the front is currently null...
         if(front == null) {
+            // ...set front and back to the new node
             front = node;
             back = node;
         } else {
+            // ...otherwise, make the new node the new back of the queue
             back.setNext(node);
             back = node;
         }
+        // increment size
         size++;
     }
 
     @Override
     public T peek() {
+        // return the value at the front of the queue
         return front.getValue();
     }
 
     @Override
     public T dequeue() {
+        // get the value from the front
         T value = front.getValue();
+        // move the front to the next node in the queue
         front = front.getNext();
+        // decrement the size
         size--;
+        // return the value
         return value;
     }
 
--- src/session/NodeStack.java
+++ src/session/NodeStack.java
@@ -1,7 +1,20 @@
 package session;
 
+/**
+ * A node-based implementation of the Stack ADT.
+ *
+ * @param <T> The type of values stored in the stack.
+ */
 public class NodeStack<T> implements Stack<T> {
+    /**
+     * The node at the top of the stack. This contains the newest value in
+     * the stack.
+     */
     private Node<T> top;
+
+    /**
+     * The number of nodes in the stack.
+     */
     private int size;
 
     public NodeStack() {
@@ -11,21 +24,29 @@ public class NodeStack<T> implements Stack<T> {
 
     @Override
     public void push(T value) {
+        // create a new node with the current top as the next node
         Node<T> node = new Node<>(value, top);
+        // make the new node the top
         top = node;
+        // increment the size
         size++;
     }
 
     @Override
     public T peek() {
+        // return the value in the top node
         return top.getValue();
     }
 
     @Override
     public T pop() {
+        // get the value in the top node
         T value = top.getValue();
+        // set the top to the next node in the stack
         top = top.getNext();
+        // decrement the size
         size--;
+        // return the value
         return value;
     }
 
